53cb4d3df2c96f509efc0c13863d83a3664202a3
HarryNelson <55327118+HarryNelson@users.noreply.github.com>
2019-10-19 10:20:48 -0500

    Initial commit

---
+++ mathmaticalOperatorsDebug.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+
+print(40 * 0 == -1) 
+    
+print("Hello 1 2 and" + 3) 
+   
+print(6* 5+ 1 == 31)  
+  
+print(10 + "/" + 2)
+         
+print("LOL" * 3)  
+      
+print("1" + "2" + "3")
+
+print(10 % 2 == 0)    
